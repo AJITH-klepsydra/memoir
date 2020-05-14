be4bb4bc280cc81294f4ec76c7e870e79360b2b2
Ajith-Klep <ajithpmuralidharan01@gmail.com>
2020-05-14 18:33:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pyrebase
 from PyQt5.QtCore import Qt,pyqtSignal,QTimer
 from PyQt5.QtCore import QSize
 from datetime import date
-
+import random
+import webbrowser
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QPalette, QBrush, QMovie 
 from firebase import firebase as fb
 import time
@@ -13,7 +15,7 @@ class main_window(QtWidgets.QMainWindow):
 	signal_dash = pyqtSignal(str,str)
 	def __init__(self):
 		super(main_window,self).__init__()
-
+		uic.loadUi('a.ui',self)
 		syle1='''
 		QPushButton{
 		background-color: rgb(88, 178, 23);
@@ -25,9 +27,6 @@ class main_window(QtWidgets.QMainWindow):
 		font: 15pt "Open Sans";
 		border-radius:1px;color: rgb(255, 255, 255);
 		}
-
-
-
 		'''
 		self.firebaseConfig ={
 			"apiKey": "AIzaSyA--z3Dm4GF7GR4hd0OAvEc-IFkHlIe5Zs",
@@ -40,28 +39,20 @@ class main_window(QtWidgets.QMainWindow):
     		"appId": "1:26624224100:web:fc61853f8851b03c2be871",
     		"measurementId": "G-44TC6S38Y4"
 		}
-
-		uic.loadUi('a.ui',self)
-		
 		self.pushButton_4.hide()
 		self.pushButton_3.hide()
 		self.pushButton.setStyleSheet(syle1)
 		self.pushButton_3.setStyleSheet(syle1)
 		self.pushButton.setShortcut('return')
 		self.pushButton_3.setShortcut('return')
-		#self.anime.hide()
-		
 		self.label_3.setStyleSheet(" background-image: url(bg.webp);")
 		self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
-		#elf.mv = QMovie('load.gif')
-		#self.anime.setMovie(self.mv)
 		self.pushButton_2.clicked.connect(self.signup_page)
 		self.pushButton.clicked.connect(self.login_ver)
 		self.password = self.lineEdit_2.text()
 		self.email = self.lineEdit.text()
 		self.show()
 		self.pushButton_3.clicked.connect(self.cloudin)
-	
 	def login_ver(self):
 		self.password = self.lineEdit_2.text()
 		self.email = self.lineEdit.text()
@@ -71,9 +62,7 @@ class main_window(QtWidgets.QMainWindow):
 		self.email = self.lineEdit.text()
 		email = self.lineEdit.text()
 		self.password = self.lineEdit_2.text()
-		with open("info.txt","w") as f:
 
-			f.write(email)
 		if(len(self.password)<6):
 			self.status.setText("WeakPassword")
 		else:
@@ -87,47 +76,45 @@ class main_window(QtWidgets.QMainWindow):
 	def signup_page(self):
 		self.password = self.lineEdit_2.text()
 		self.email = self.lineEdit.text()
-		#self.anime.show()
-		#self.mv.start()
-		self.label.setText("Sign up")
-
+		self.label.setText("Sign Up ")
 		self.label_2.setText(" ")
 		self.pushButton_2.hide()
-		
 		self.pushButton.hide()
 		self.pushButton_3.show()
+
 		self.widget_3.setStyleSheet("background-color:rgb(144, 0, 72);")	
-		#self.mv.stop()
-		#self.anime.hide()
 	def cloudin(self):
 		self.password = self.lineEdit_2.text()
 		self.email = self.lineEdit.text()
-		with open("info.txt","w") as f:
-			f.write(email)
-		if(len(password)<6):
+
+		if(len(self.password)<6):
 			self.status.setText("WeakPassword")
 		else:
 			try:
-				#self.anime.show()
-				#self.mv.start()
 				firebase = pyrebase.initialize_app(self.firebaseConfig)
 				auth = firebase.auth()
-				user = auth.create_user_with_email_and_password(email,password)
-				#timer= QTimer(self)
-				#timer.singleShot(5000,self.stopan)
-				self.signal_dash.emit(self.password)
-				#self.anime.hide()
+				user = auth.create_user_with_email_and_password(self.email,self.password)
+				self.signal_dash.emit(self.password,self.email)
 			except Exception as e:
-				print(e)
-				#self.mv.stop()
-				#self.anime.hide()
 				self.status.setText("User Exists")
-
-
-
 class dash_window(main_window):
 	def __init__(self,passw,emil):
 		super(dash_window,self).__init__()
+		style1='''
+			QPushButton{
+			font: 120pt "OpenSymbol";
+			background-color: rgb(97, 168, 124);
+			color: rgb(22, 37, 44);
+			border-radius:75px;
+			}
+			QPushButton:hover{
+			font: 120pt "OpenSymbol";
+			background-color: rgb(203, 232, 148);
+			color: rgb(171, 69, 22);
+			border-radius:75px;
+			}
+		'''
+
 		uic.loadUi("dash.ui",self)
 		self.subm.hide()
 		self.rest.hide()
@@ -141,9 +128,30 @@ class dash_window(main_window):
 		self.subm_2.hide()
 		self.label_3.hide()
 		self.label_5.hide()
+		ls_q=["\" Each new Day is blank page in the diary of your life.The Secret of Success is in turning that diary in to the best story you possibly can \"",
+			  "\"I Can shake of everything as I write, my sorrows disappear, my courage is reborn\"",
+			  "\" Writing a diary is like living your memories at present\"",
+   			  "\"The life of every person is like a diary in which he means to write one and writes another \"",
+              "\" Dear Diary, Oh it's all too much to explain and you wouldn't believe it anyway I'm going to bed\"",
+              "\"I really don't know what's happened to me, but I have changed from top to toe. I am living in a strange mixture of memories of yesterday and today.\"",
+              "\" Memory.. is the diary that we all carry with us\"",
+              "\"I'm actually not funny.I'm just mean and people think i'm joking \"",
+              "\"Once I loved her.. : ( \"",
+              "\"Is it me... (please let it be me)\"",
+              "\"Every night, I laid awake with your memories flooding through my eyes with the hope to be with you when sleep arrived.\"",
+              "\"I will kill myself soon. But until then how do l tame my pain?\"",
+              "\"I stand still inhaling the beauty of our memories,Flashbacks of our togetherness burn my flesh and I breathe love through every single skin pore. \"",
+              "\"Sometimes things are better left unsaid until you're free of them.\"",
+              "\"Never Love Someone Over Your Nerves! And You Can't even Help it Happening \""]
+		self.textBrowser.setPlainText(ls_q[random.randint(0,len(ls_q)-1)])
+		self.textBrowser.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:1 rgba(0, 0, 0, 0));border-radius:10px;font: 57 20pt \"Tlwg Typewriter\";color: rgb(135, 155, 99);")
+		self.pushButton.setStyleSheet(style1)
+		#self.pushButton_5.setStyleSheet(style2)
+		self.pushButton_7.clicked.connect(self.editor)
 		self.msgb.hide()
 		self.comboBox.hide()
 		self.cl.clicked.connect(self.retrieve)
+		self.pushButton_6.clicked.connect(self.info)
 		self.firebaseConfig ={
 			"apiKey": "AIzaSyA--z3Dm4GF7GR4hd0OAvEc-IFkHlIe5Zs",
     		"authDomain": "memoir-journal-app.firebaseapp.com",
@@ -163,10 +171,15 @@ class dash_window(main_window):
 		self.pushButton.clicked.connect(self.editor)
 		self.img.setStyleSheet(" background-image: url(userp.png);border-width:2px;border-radius:40px;")
 		with open("info.txt","r") as f:
-			self.usern.setText("  "+str(str(f.read()).split('@')[0]))
+			self.usern.setText("  "+str(self.email.split('@')[0]))
 		self.show()
+	def info(self):
+		self.obj = info()
+		self.obj.show()
+		self.obj.setWindowTitle("Info")
+
 	def retrieve(self):
-		
+		QtWidgets.QApplication.setOverrideCursor(Qt.WaitCursor)
 		email= self.email
 		self.mv= QMovie('sad1.gif')
 		self.mv1= QMovie('ecstatic.gif')
@@ -175,11 +188,10 @@ class dash_window(main_window):
 		auth = firebase.auth()
 		user1 = auth.sign_in_with_email_and_password(self.email,self.password)
 		stri= str(user1['localId'])
-		firebase = fb.FirebaseApplication("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls[0]+'-'+self.ls[1],None)
-
 		curr_date = str(self.cl.selectedDate().toString("yyyy/MM/dd"))
-		
-		result = firebase.get("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls[0]+'-'+self.ls[1]+":",'')
+		self.ls1 = curr_date.split('/')
+		firebase = fb.FirebaseApplication("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls1[0]+'-'+self.ls1[1],None)
+		result = firebase.get("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls1[0]+'-'+self.ls1[1]+":",'')
 		
 		self.f1.hide()
 		#self.label5.hide()
@@ -191,13 +203,14 @@ class dash_window(main_window):
 		self.rest_2.show()
 		self.rest.show()
 		self.resb.show()
-		
+		count =0
 		for x,y in result.items():
 				for key in y:
 
 				#print(str(y[key]))
 				
 					if(str(y[key])==curr_date):
+						count=count+1
 						
 						self.label_6.show()
 						self.label_6.setMovie(self.mv)
@@ -206,8 +219,15 @@ class dash_window(main_window):
 						self.rest_2.setText(str(y['date']))
 						self.rest.setText(str(y['title']))
 						self.resb.setPlainText(str(y['message']))
+		if(count ==0 ):
+			self.label_6.show()
+
+			self.rest_2.setText("No data")
+			self.rest.setText('No data')
+			self.resb.setPlainText('No data')
+
 			
-		#print(result)
+		QtWidgets.QApplication.restoreOverrideCursor()
 	def editor(self):
 		self.comboBox.show()
 		self.subm_2.show()
@@ -218,12 +238,14 @@ class dash_window(main_window):
 		self.rest_2.hide()
 		self.resb.hide()
 		self.subm.show()
+		self.subm.setText("submit")
 		self.label_3.show()
 		self.textBrowser.hide()
 		self.msgb.show()
 		self.subm.clicked.connect(self.tocloud)
 		self.label_5.setText("  "+self.ls[2]+"/"+self.ls[1]+"/"+self.ls[0])
 	def tocloud(self):
+		QtWidgets.QApplication.setOverrideCursor(Qt.WaitCursor)
 		firebase = pyrebase.initialize_app(self.firebaseConfig)
 		auth = firebase.auth()
 		
@@ -240,9 +262,21 @@ class dash_window(main_window):
 		email_ls = email.split('@')
 		firebase = fb.FirebaseApplication("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls[0]+'-'+self.ls[1],None)
 		result = firebase.post("https://memoir-journal-app.firebaseio.com/"+email_ls[0]+self.password+'/'+self.ls[0]+'-'+self.ls[1]+":",data)
-		print("success")
+		self.subm.setText("done")
+		QtWidgets.QApplication.restoreOverrideCursor()
+		
 
-
+class info(main_window):
+	def __init__(self):
+		super(info,self).__init__()
+		uic.loadUi('info.ui',self)
+		self.fb_6.clicked.connect(self.fb1)
+		self.mv = QMovie('esctatic.gif')
+		self.bgl.setMovie(self.mv)
+		self.mv.start()
+		self.show()
+	def fb1(self):
+		webbrowser.open("https://github.com/AJITH-klepsydra/memoir")
 
 
 class controller():
@@ -253,9 +287,13 @@ class controller():
 		self.sin1 = main_window()
 		self.sin1.show()
 		self.sin1.signal_dash.connect(self.to_dash)
+		self.sin1.setWindowTitle("Login")
 	def to_dash(self,passw,emil):
+		self.sin1.hide()
 		self.dash1 = dash_window(passw,emil)
+		self.dash1.setWindowTitle("Memoir")
 		self.dash1.show()
+
 
 app = QtWidgets.QApplication(sys.argv)
 win = controller()
